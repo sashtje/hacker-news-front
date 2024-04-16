2307ff3c587bffeb13b7f1f69f87ae045d956b46
sashtje <sashtje.lefevre@gmail.com>
2024-04-16 18:01:03 +0300

    feat: add header

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+
+import { getRouteMain } from "../../shared/const/router";
+
+import cls from "./Header.module.css";
+
+import { ReactComponent as LogoIcon } from "../../shared/assets/icons/logo.svg";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={cls.header}>
+      <Link to={getRouteMain()}>
+        <LogoIcon width={60} />
+      </Link>
+      Hacker News
+    </header>
+  );
 };
